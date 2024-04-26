e3876fd7326d214d98db27629ed814b5b3533814
raufkhalilov <rauf.khalilov.2004@mail.ru>
2024-04-26 19:06:50 +0300

    Main code

---
+++ ThreadsMutex.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx;
+int sharedData = 0; // это общая переменная для чтения и записи
+
+void writerFunction() {
+    for (int i = 0; i < 5; ++i) {
+        mtx.lock();
+        sharedData = i;
+        std::cout << "Writer wrote: " << sharedData << std::endl;
+        mtx.unlock();
+
+        // Задержка для наглядности
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+void readerFunction() {
+    for (int i = 0; i < 5; ++i) {
+        mtx.lock();
+        std::cout << "Reader read: " << sharedData << std::endl;
+        mtx.unlock();
+
+        // Задержка для наглядности
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+int main() {
+    std::thread writerThread(writerFunction);
+    std::thread readerThread(readerFunction);
+
+    writerThread.join();
+    readerThread.join();
+
+    return 0;
+}
